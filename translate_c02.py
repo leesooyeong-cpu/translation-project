f1f1070d8f93c5fe8ec22dc0ee708856b4bef08f
Lee Sooyeong <leesooyeong@gmail.com>
2026-05-17 17:02:40 +0900

    Auto: translation update 2026-05-17 17:02

--- translate_c02.py
+++ translate_c02.py
@@ -69,7 +69,7 @@ def translate_pptx(input_path, output_path):
     prs.save(output_path)
 
 def main():
-    src_dir = Path(r"C:\hex_edit_project\VN_original\025 Máy cắt tự động_C02")
+    src_dir = Path(r"C:\hex_edit_project\VN_original\026_Tiêu chuẩn_Công đoạn_Dây Sheild")
     out_dir = Path(r"C:\hex_edit_project\KO_result")
     out_dir.mkdir(exist_ok=True)
 
